cd443af722150d62892febdeeb5dea2bf569fec0
zkl <352522818@qq.com>
2015-09-07 18:38:39 +0800

    增加文章

--- _drafts/文章集锦/merge.sh
+++ _drafts/文章集锦/merge.sh
@@ -6,6 +6,6 @@ DOCFILE=articles
 
 cat $DOCPATH/* > $DOCFILE.md
 
-pandoc -o $DOCFILE.epub -t epub3 --epub-stylesheet=css/stylesheet.css $DOCFILE.md
+pandoc -o $DOCFILE.epub -t epub3 --toc --toc-depth=1 --epub-stylesheet=css/style.css $DOCFILE.md
 
 kindlegen $DOCFILE.epub

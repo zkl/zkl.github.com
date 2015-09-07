c5762b3554e78a3d5f37904a27b7959400656258
zkl <352522818@qq.com>
2015-09-07 13:55:39 +0800

    更新文章

--- _drafts/文章集锦/merge.sh
+++ _drafts/文章集锦/merge.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-DOCPATH=文档
-DOCFILE=文章集锦.md
+DOCPATH=document
+DOCFILE=articles
 
 
-cat $DOCPATH/* > $DOCFILE
+cat $DOCPATH/* > $DOCFILE.md
+
+pandoc -o $DOCFILE.epub -t epub3 --epub-stylesheet=css/stylesheet.css $DOCFILE.md
+
+kindlegen $DOCFILE.epub
